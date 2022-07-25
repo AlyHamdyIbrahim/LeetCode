768b4f0bc45cd68f4aa537c8c9a6f8a3ae682b86
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2022-07-25 17:58:13 +0200

    Update palindrome_number_9.py

--- palindrome_number_9.py
+++ palindrome_number_9.py
@@ -3,9 +3,8 @@ class Solution:
         if x < 0:
             return False
         numarray = list(map(int, str(x)))
-        print(numarray)
         l_numarray = len(numarray)
-        for i in range(l_numarray):
+        for i in range(l_numarray//2):
             if numarray[l_numarray-i-1] != numarray[i]:
                 return False
         return True
