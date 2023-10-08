48e5926a0078feb8e31baffdc26f45d3fab080c6
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2023-10-08 16:20:08 +0300

    Update 20_valid_parentheses.py

--- 20_valid_parentheses.py
+++ 20_valid_parentheses.py
@@ -1,2 +1,23 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        sLength = len(s)
+        if sLength % 2 != 0:
+            return False
+        
+        stack = []
+
+        for i in range(sLength):
+            if s[i] == '(' or s[i] == '{' or s[i] == '[':
+                stack.append(s[i])
+            else:
+                if len(stack) == 0:
+                    return False
+                if s[i] == ')' and stack[-1] != '(':
+                    return False
+                if s[i] == '}' and stack[-1] != '{':
+                    return False
+                if s[i] == ']' and stack[-1] != '[':
+                    return False
+                stack.pop()
+
+        return True if len(stack) == 0 else False
