fb6ddd34bbc218094f583e062f4d9f672e1adc45
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2023-10-08 16:04:13 +0300

    Update 14_longest_common_prefix.py

--- 14_longest_common_prefix.py
+++ 14_longest_common_prefix.py
@@ -1,2 +1,20 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+    def longestCommonPrefix(self, strs: [str]) -> str:
+        match = True
+        shortestLength = 100
+        listLength = len(strs)
+        for i in range(listLength):
+            if len(strs[i]) < shortestLength:
+                shortestLength = len(strs[i])
+        i = 0
+        while match and (i < shortestLength):
+            for j in range(listLength - 1):
+                if strs[j][i] != strs[j + 1][i]:
+                    match = False
+                    break
+            if match:
+                i += 1
+        print(i)
+        return strs[0][:i]
+    
+print(Solution().longestCommonPrefix(["flower","flow","flight"]))
