f433624b3dc52ab39b5ec7de5aa59c442e005be0
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2023-10-09 15:14:58 +0300

    Update 35_search_insert_position.py

--- 35_search_insert_position.py
+++ 35_search_insert_position.py
@@ -1,3 +1,20 @@
 class Solution:
     def searchInsert(self, nums: [int], target: int) -> int:
+        r = len(nums) - 1
+        l = 0
+
+        while l <= r:
+            
+            mid = l + (r - l) // 2
+
+            if nums[mid] == target:
+                return mid
+            
+            if nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
         
+        return mid + 1 if nums[mid] < target else mid
+
+print(Solution().searchInsert([1,3,5,6], 4))
