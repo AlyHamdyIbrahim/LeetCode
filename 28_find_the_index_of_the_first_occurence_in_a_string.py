84e4ae92b4cf808677f8d4e2e2a7270d8557e8ee
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2023-10-09 15:05:42 +0300

    Update 28_find_the_index_of_the_first_occurence_in_a_string.py

--- 28_find_the_index_of_the_first_occurence_in_a_string.py
+++ 28_find_the_index_of_the_first_occurence_in_a_string.py
@@ -1,3 +1,13 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        haystackLength = len(haystack)
+        needleLength = len(needle)
+
+        if needleLength > haystackLength:
+            return -1
         
+        for i in range(haystackLength - needleLength + 1):
+            if haystack[i:i+needleLength] == needle:
+                return i
+        
+        return -1
