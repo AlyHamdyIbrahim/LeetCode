7d3b82f1bb0f43b4ae3efead764467a4ed766849
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2023-12-07 14:03:11 +0200

    Update 169_majority_element.py

--- 169_majority_element.py
+++ 169_majority_element.py
@@ -1,3 +1,24 @@
 class Solution:
     def majorityElement(self, nums: [int]) -> int:
+        appearances = dict()
+        for num in nums:
+            if num in appearances:
+                appearances[num] += 1
+            else:
+                appearances[num] = 1
         
+        for num in appearances:
+            if appearances[num] > len(nums) / 2:
+                return num
+            
+    def majorityElement2(self, nums: [int]) -> int:
+        c = 0
+        majority = None
+        for i in range(len(nums)):
+            if c == 0:
+                majority = nums[i]
+            if nums[i] == majority:
+                c += 1
+            else:
+                c -= 1
+        return majority
