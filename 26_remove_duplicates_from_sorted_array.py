ee05805504d6952896067a1c5583f79604ffab0f
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2023-12-07 13:09:19 +0200

    Update 26_remove_duplicates_from_sorted_array.py

--- 26_remove_duplicates_from_sorted_array.py
+++ 26_remove_duplicates_from_sorted_array.py
@@ -9,8 +9,6 @@ class Solution:
             if nums[i] != nums[j]:
                 i += 1
                 nums[i] = nums[j]
-                j += 1
-            else:
-                j += 1
+            j += 1
         return i + 1
     
