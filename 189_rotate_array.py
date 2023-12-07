ccb8ce8aa67c2ee0a6b6f0ade096ae70995725a3
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2023-12-07 15:52:02 +0200

    Update 189_rotate_array.py

--- 189_rotate_array.py
+++ 189_rotate_array.py
@@ -1,5 +1,17 @@
 class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
+    def rotate(self, nums: [int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
+        length = len(nums)
+        k = k % length
+        nums = nums[0:length-k][::-1] + nums[length-k:][::-1]
+        nums = nums[::-1]
+
+    def rotate2(self, nums: [int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        k = k % length
+        nums = nums[length-k:] + nums[:length-k]
