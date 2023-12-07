31a5049b80524d4f7d6704d34243ca864b586205
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2023-12-07 12:55:12 +0200

    Update 27_remove_element.py

--- 27_remove_element.py
+++ 27_remove_element.py
@@ -13,3 +13,16 @@ class Solution:
                 nums[i] = nums[j]
                 j += 1
         return i + 1
+    
+    def removeElement2(self, nums: [int], val: int) -> int:
+        numsLength = len(nums)
+        if numsLength == 0:
+            return 0
+        i = 0
+        j = numsLength - 1
+        while i <= j:
+            if nums[i] == val:
+                nums[i] = nums[j]
+                j -= 1
+            else:
+                i += 1
