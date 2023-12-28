98cecad3db123df3420f40d42ae82945f15dc7b5
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2023-12-28 15:36:53 +0200

    Update 70_climbing_stairs.py

--- 70_climbing_stairs.py
+++ 70_climbing_stairs.py
@@ -1,3 +1,13 @@
 class Solution:
+    # Iterative approach
     def climbStairs(self, n: int) -> int:
-        
+        x, y = 1, 1
+        for _ in range(n-1):
+            x, y = y, x + y
+        return y
+
+    # Recursive approach
+    def climbStairs2(self, n: int) -> int:
+        if n <= 2:
+            return n
+        return self.climbStairs2(n - 1) + self.climbStairs2(n - 2)
