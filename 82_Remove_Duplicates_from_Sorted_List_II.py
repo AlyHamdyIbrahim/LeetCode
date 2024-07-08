81b877a6506eae0cfaf43d142c853103a76829fb
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2024-07-08 16:36:01 +0300

    Update 82_Remove_Duplicates_from_Sorted_List_II.py

--- 82_Remove_Duplicates_from_Sorted_List_II.py
+++ 82_Remove_Duplicates_from_Sorted_List_II.py
@@ -5,3 +5,23 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        returnHead = ListNode()
+        previous = returnHead
+        current = head
+        stack = []
+        while (current != None):
+            if stack and current.val != stack[-1].val:
+                if len(stack) > 1:
+                    stack = []
+                else:
+                    previous.next = stack.pop()
+                    previous = previous.next
+            stack.append(current)
+            current = current.next
+
+        if len(stack) > 1:
+            previous.next = None
+        if len(stack) == 1:
+            previous.next = stack.pop()
+
+        return returnHead.next
